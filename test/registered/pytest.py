9352670d8b2c53b657e59a9c6ae09ace0f15bc80
sara010120 <sara01012026@outlook.com>
2026-03-09 00:04:11 -0700

    Update pytest.py

--- test/registered/pytest.py
+++ test/registered/pytest.py
@@ -5,3 +5,4 @@ class CommandRunner:
     def run_cmd(self):
         result = subprocess.run("echo 1>/tmp/1", shell=True, capture_output=True, text=True)
         return result.stdout
+CUDA_SUITE_TO_RUNNER(stage-a-test-1,suite="stage-a-test-1")
